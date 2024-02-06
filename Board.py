0bdf54bf328ea10ca23d63b6d53113ac22d57c5b
izorginpasha <izorgin.pasha@yandex.ru>
2024-02-06 10:31:55 +0500

    коммÐструктура без реализÐации

---
+++ Board.py
@@ -0,0 +1,23 @@
+class Board:
+    def __int__(self):
+        self.status = [[]]
+        self.ship_maps = []
+        self.hid = False
+        self.count_shift = int
+    def add_ship(self):# ставит корабль на доску
+        try:
+        except:
+        else:
+        finally:
+    def contour(self):# обводит корабль по контуру
+
+    def next_map(self):#  выводит доску в консоль в зависимости от параметра hid
+
+    def out(self):# для точки (объекта класса Dot)
+
+
+    def shot(self):#выстрел по доске
+
+
+
+
---
+++ Ship.py
@@ -0,0 +1,11 @@
+class Ship:
+    def __init__(self, length_, start, direction, life):
+        self.length_ = length_
+        self.start = start
+        self.direction = direction
+        self.life = life
+
+    def dots(self): #возвращает список всех точек корабля.
+
+        list = []
+        return list
