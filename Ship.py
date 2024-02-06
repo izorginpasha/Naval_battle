3aa81c8819d018253d32f21bbe4f654c64e20259
izorginpasha <izorgin.pasha@yandex.ru>
2024-02-06 13:42:07 +0500

    комментарÐShip good

--- Ship.py
+++ Ship.py
@@ -6,6 +6,18 @@ class Ship:
         self.life = life
 
     def dots(self): #возвращает список всех точек корабля.
-
         list = []
+
+        for i in range(self.length_):
+            if self.direction == "x+":
+               list.append(self.start[0]+i)
+            elif self.direction == "x-":
+                list.append(self.start[0]-i)
+            elif self.direction == "y+":
+                list.append(self.start[1] + i)
+            elif self.direction == "y-":
+                list.append(self.start[1] - i)
+
         return list
+a = Ship(3,[1.2],"x+",3)
+print(a.dots())
