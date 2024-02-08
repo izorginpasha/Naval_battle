6c145950611758121ab0ac9e70ad0a591b53455f
izorginpasha <izorgin.pasha@yandex.ru>
2024-02-08 13:49:47 +0500

    комментарÐout

--- Board.py
+++ Board.py
@@ -1,6 +1,7 @@
 from Ship import Ship
 
 
+
 class Board:
     _status = [[0] * 6 for i in range(6)]
     ship_maps = []
@@ -16,7 +17,8 @@ class Board:
             ship = Ship(ship_len, ship_start, ship_direction, ship_len)
             print(ship.dots())
             for i in ship.dots():
-                self.status[i.x][i.y] = u'\u2588'
+                if (self.out(i) == False):
+                    self.status[i.x][i.y] = u'\u2588'
             self.contour(ship.dots())
 
         except:
@@ -25,13 +27,9 @@ class Board:
 
     def contour(self, ship_list):  # обводит корабль по контуру
         for i in ship_list:
-            print(i.x, i.y)
-            d = i.x + 2
-            c = i.y + 2
-            for x in range(i.x-1, i.x+2):
-                for y in range(i.y-1, i.y+2):
-                    print(x, y)
-                    if (self.status[x][y] != u'\u2588'):
+            for x in range(i.x - 1, i.x + 2):
+                for y in range(i.y - 1, i.y + 2):
+                    if (self.status[x][y] != u'\u2588' and x>=0 and y>=0):
                         self.status[x][y] = "*"
 
     def next_map(self):  # выводит доску в консоль в зависимости от параметра hid
@@ -46,7 +44,12 @@ class Board:
 
         return ""
 
-    # def out(self):# для точки (объекта класса Dot)
+    def out(self, dot):  # для точки (объекта класса Dot)
+        if (dot.x < 0 and dot.y < 0):
+            return True
+        else:
+            return False
+
     @status.setter
     def status(self, value):
         self._status = value
@@ -58,5 +61,5 @@ class Board:
 
 
 a = Board()
-a.add_ship(2, [1, 2], "x+")
+a.add_ship(3, [3, 3], "x+")
 a.next_map()
