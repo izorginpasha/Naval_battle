bb7480559b602207ae0b2104128f159713e6960f
izorginpasha <izorgin.pasha@yandex.ru>
2024-02-08 12:11:16 +0500

    комментарий к коммиту

--- Board.py
+++ Board.py
@@ -14,17 +14,29 @@ class Board:
     def add_ship(self, ship_len, ship_start, ship_direction):  # ставит корабль на доску
         try:  # u'\u2588'
             ship = Ship(ship_len, ship_start, ship_direction, ship_len)
+            print(ship.dots())
             for i in ship.dots():
-
-                self.status[int(i[0])][int(i[1])] = u'\u2588'
+                self.status[i.x][i.y] = u'\u2588'
+            self.contour(ship.dots())
 
         except:
             print("Ошибка постановки корабля")
+            self.status = [[0] * 6 for i in range(6)]
 
-    # def contour(self):# обводит корабль по контуру
+    def contour(self, ship_list):  # обводит корабль по контуру
+        for i in ship_list:
+            print(i.x, i.y)
+            d = i.x + 2
+            c = i.y + 2
+            for x in range(i.x-1, i.x+2):
+                for y in range(i.y-1, i.y+2):
+                    print(x, y)
+                    if (self.status[x][y] != u'\u2588'):
+                        self.status[x][y] = "*"
 
     def next_map(self):  # выводит доску в консоль в зависимости от параметра hid
         if self.hid == True:
+            print(self.status)
             for i in range(len(self.status)):
                 if i == 0: print("  1 2 3 4 5 6")
                 for j in range(len(self.status)):
@@ -35,6 +47,9 @@ class Board:
         return ""
 
     # def out(self):# для точки (объекта класса Dot)
+    @status.setter
+    def status(self, value):
+        self._status = value
 
 
 #
@@ -43,5 +58,5 @@ class Board:
 
 
 a = Board()
-a.add_ship(2, [2, 1], "x+")
-print(a.next_map())
+a.add_ship(2, [1, 2], "x+")
+a.next_map()
--- Ship.py
+++ Ship.py
@@ -1,3 +1,6 @@
+from Dot import Dot
+
+
 class Ship:
     def __init__(self, length_, start, direction, life):
         self.length_ = length_
@@ -7,19 +10,31 @@ class Ship:
 
     def dots(self):  # возвращает список всех точек корабля.
         list = []
+        try:
+            for i in range(self.length_):
 
-        for i in range(self.length_):
-            if self.direction == "y+":
-                list.append([self.start[0] + i-1, self.start[1]-1])
-            elif self.direction == "y-":
-                list.append([self.start[0] - i-1, self.start[1]-1])
-            elif self.direction == "x+":
-                list.append([self.start[1] + i-1, self.start[0]-1])
-            elif self.direction == "x-":
-                list.append([self.start[1] - i-1, self.start[0]-1])
-
-        return list
+                if self.direction == "x+":
+                    list.append(self.dot(self.start[1] - 1, self.start[0] + (i - 1)))
+                elif self.direction == "x-":
+                    list.append(self.dot(self.start[1] - 1, self.start[0] - (i + 1)))
+                elif self.direction == "y+":
+                    list.append(self.dot(self.start[1] + i - 1, self.start[0] - 1))
+                elif self.direction == "y-":
+                    list.append(self.dot(self.start[1] - i - 1, self.start[0] - 1))
 
 
-# a = Ship(3, [1, 2], "x+", 3)
-# print(a.dots())
+            return list
+        except:
+            return False
+    def dot(self, point_x, point_y):
+        try:
+            if (point_x >= 0 and point_y >= 0):
+                return Dot(point_x, point_y)
+            else:
+                return exit()
+        except:
+            print("ошибка точки")
+
+# a = Ship(3, [1, 1], "x+", 3)
+# b = a.dots()
+# print(b)
